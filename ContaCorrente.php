caf9f84dda6df3e05c936c0572e6adc3bb75c193
Alura Verde <aluraverde@Alura-Verde.home>
2019-01-14 21:22:46 -0200

    metodos staticos

---
+++ ContaCorrente.php
@@ -0,0 +1,101 @@
+<?php
+
+
+class ContaCorrente{
+
+	private $titular;
+	
+	public  $agencia;
+	
+	private $numero;
+	
+	private $saldo;
+
+	public function __construct($titular,$agencia,$numero,$saldo){
+
+		$this->titular = $titular;
+		$this->agencia = $agencia;
+		$this->numero = $numero;
+		$this->saldo = $saldo;
+
+	}
+
+
+
+	public function sacar($valor){
+
+		Validacao::verificaNumerico($valor);
+
+		$this->saldo = $this->saldo - $valor;
+		return $this;
+
+	}
+	
+	public function depositar($valor){
+		
+		Validacao::verificaNumerico($valor);
+
+		$this->saldo = $this->saldo + $valor;
+		return $this;
+
+	}
+
+	public function transferir($valor, ContaCorrente $contaCorrente){
+
+		Validacao::verificaNumerico($valor);
+
+		$this->sacar($valor);
+
+		$contaCorrente->depositar($valor);
+
+		return $this;
+
+
+	}
+
+
+	public function __get($atributo){
+
+		Validacao::protegeAtributo($atributo);
+		
+		return $this->$atributo;
+
+	}
+
+
+	public function __set($atributo,$valor){
+
+		Validacao::protegeAtributo($atributo);
+
+		$this->$atributo = $valor;
+
+	}
+
+
+	
+	
+	public function setNumero($numero){
+		return $this->numero = $numero;
+	}
+
+
+
+
+
+	private function formataSaldo(){
+
+		return "R$ ".number_format($this->saldo,2,",",".");
+	}
+
+
+	public function getSaldo(){
+		return $this->formataSaldo();
+	}
+
+	public function __toString(){
+
+		return $this->saldo;
+	}
+
+
+}
--- index.php
+++ index.php
@@ -1,35 +1,57 @@
 <?php
 
-ini_set('display_errors',1);
-error_reporting(E_ALL);
-header('Content-Type: text/html; charset=utf-8');
-
-
-require_once "Entity/ContaCorrente.php";
-
-
-$contaCorrenteJoao = new ContaCorrente("João Lucas","5199","16321-2", 500.50);
-
-$contaCorrenteJose = new ContaCorrente("Jose da silva","5199","122221-2", 900.50);
-
-
-echo "Inicio da conta dos correntistas joao e jose<br />";
-echo $contaCorrenteJoao;
-
-echo $contaCorrenteJose;
-
-echo "<br />";
-
-
-$contaCorrenteJoao->sacar(20.00);
-echo $contaCorrenteJoao; 
-
-$contaCorrenteJoao->depositar(20.00);
-echo $contaCorrenteJoao; 
+ ini_set('display_errors',1);
+ error_reporting(E_ALL);
+ header('Content-Type: text/html; charset=utf-8');
 
 
 
-$contaCorrenteJoao->transferir(10.00, $contaCorrenteJose);
-echo $contaCorrenteJoao; 
+require "Validacao.php";
+require "ContaCorrente.php";
 
-echo $contaCorrenteJose; 
+
+
+
+$contaJoao = new ContaCorrente("Joao","1212","343477-9",2000.00);
+$contaMaria = new ContaCorrente("Maria","1212","343423-9",6000.00);
+
+$contaJoao->sacar("33");
+
+var_dump($contaJoao);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
